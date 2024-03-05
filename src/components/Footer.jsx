94282a944ebb9d5cc1be6955f1b9b838fa3ae5d1
rsshejole <rsshejole7@gmail.com>
2024-03-05 19:54:29 +0530

    post form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import Logo from '../image/logo.png'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return (
+    return (
     <div className='footer'>
         <img src={Logo}/>
         <span><Link to='/feedback'>Leave us a review</Link></span>
     </div>
-  )
+    )
 }
 
 export default Footer
